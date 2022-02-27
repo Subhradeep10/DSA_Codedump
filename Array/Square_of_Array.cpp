fbbed9de664394501208f2387ab0d7dbeeb3757c
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-02-27 13:33:11 +0530

    Maxproduct of 3 number added

---
+++ Array/Square_of_Array.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    vector<int> sortedSquares(vector<int> &nums)
+    {
+        vector<int> v;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            v.push_back(nums[i] * nums[i]);
+        }
+        sort(v.begin(), v.end());
+        return v;
+    }
+};
+
+// Leetcode Link
+//  https://leetcode.com/problems/squares-of-a-sorted-array/

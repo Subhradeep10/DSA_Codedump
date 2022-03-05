55eea95a9c230b32e9475cc556e1a6d4bfda3fdc
Subradeep10 <subhradeepsamanta2002@gmail.com>
2022-03-05 20:22:48 +0530

    Math file added

---
+++ Math/Number Complement.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int findComplement(int num)
+    {
+        if (num == 0)
+            return 1;
+        unsigned mask = ~0;
+        while (mask & num)
+            mask = mask << 1;
+        return ~num ^ mask;
+    }
+};
